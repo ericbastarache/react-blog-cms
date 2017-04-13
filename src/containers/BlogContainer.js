241e85cd63a9f34d94654b82f8bb6fbea20ab74a
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-13 17:31:42 -0400

    Created blog container with dummy data

---
+++ src/containers/BlogContainer.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Blog from '../components/Blog';
+import { connect } from 'react-redux';
+
+export class BlogContainer extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      posts: [
+        {
+          name: 'Test post'
+        },
+        {
+          name: 'Another test post'
+        },
+        {
+          name: 'Will the test posts ever end?'
+        }
+      ]
+    }
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <Blog posts={this.state.posts} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps)(BlogContainer);
