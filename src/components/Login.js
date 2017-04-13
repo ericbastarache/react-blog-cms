2246ea22090cf9602a855ca10a9082afa9a793d2
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-12 23:11:17 -0400

    Created preliminary login component

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginContainer from '../containers/LoginContainer';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>This is Login</h1>
+      <LoginContainer />
+    </div>
+  );
+}
+
+export default Login;
