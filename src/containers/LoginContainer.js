be7d9ee96179e643f3e548a32563297cb8314a98
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-13 17:31:56 -0400

    Created Login container with dummy data

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -4,10 +4,37 @@ import { connect } from 'react-redux';
 class LoginContainer extends Component {
   constructor (props) {
     super(props);
+    this.state = {
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  render () {
+    return (
+      <form className="form-horizontal" onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <label for="txtUsername">Enter your username:</label>
+          <input onChange={this.handleChange} name="txtUsername" className="form-control" type="text" placeholder="Enter username" />
+        </div>
+      </form>
+    );
   }
 }
 
-const mapStateToProps(state) {
+const mapStateToProps = (state) => {
   return {
     user: state.user
   }
