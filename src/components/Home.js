bce54b363dfe9fc60b17c4ca92e5a54eaa342cbe
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-13 17:33:35 -0400

    Created preliminary Home component

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="text-center">This is Home</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
