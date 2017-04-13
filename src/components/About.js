6d6c1f0e3903e739c187faa92e8856b7927049ab
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-13 17:32:55 -0400

    Created dummy About component

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="text-center">This is About</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
