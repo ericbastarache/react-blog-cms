73ba14dfd216f5997b43f5119577139d5dc3cdb3
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-13 17:32:06 -0400

    Updated entry point to use Redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
