2097390cc2222e42d8aab0ce4fb1514a34a25423
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-14 17:58:12 -0400

    Updated login styling

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './Login.css';
 import LoginContainer from './LoginContainer';
 
 const Login = () => {
   return (
-    <div>
-      <h3 className="text-center">Login</h3>
-      <LoginContainer />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 col-md-offset-2">
+          <div className="login-card">
+            <h3 className="text-center">Login</h3>
+            <LoginContainer />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
