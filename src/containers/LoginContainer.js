29a5a77246f982a93722999cb3954bc7a3dde627
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-14 16:57:53 -0400

    Added better structure to the login form and updated initial state

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -5,15 +5,14 @@ class LoginContainer extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      user: {
-        username: '',
-        password: ''
-      }
+      username: '',
+      password: ''
     }
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
+    console.log(this.state);
   }
 
   handleChange = (e) => {
@@ -24,19 +23,40 @@ class LoginContainer extends Component {
 
   render () {
     return (
-      <form className="form-horizontal" onSubmit={this.handleSubmit}>
-        <div className="form-group">
-          <label for="txtUsername">Enter your username:</label>
-          <input onChange={this.handleChange} name="txtUsername" className="form-control" type="text" placeholder="Enter username" />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3">
+            <form className="form-horizontal" onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label className="col-sm-2" htmlFor="txtUsername">Username:</label>
+                <div className="col-sm-10">
+                  <input onChange={this.handleChange} name="username" className="form-control" type="text" placeholder="Enter username" />
+                </div>
+              </div>
+              <div className="form-group">
+                <label className="col-sm-2" htmlFor="txtPassword">Password:</label>
+                <div className="col-sm-10">
+                  <input onChange={this.handleChange} name="password" className="form-control" type="password" placeholder="Enter password" />
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="col-sm-offset-2 col-sm-10">
+                  <button type="submit" className="btn btn-default">Sign In</button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-      </form>
+      </div>
+
     );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    user: state.user
+    username: state.username,
+    password: state.password
   }
 }
 
