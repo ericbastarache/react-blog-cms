14cb8ac6157d4ab5b220c3a884210466fbed1ad6
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-14 21:43:25 -0400

    Added styling to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Footer.css';
 
 const Footer = ({footerText}) => {
   return (
-    <footer>
+    <footer className="footer">
       <div className="container">
-        <p>{footerText}</p>
+        <p className="text-center">{footerText}</p>
       </div>
     </footer>
   );
