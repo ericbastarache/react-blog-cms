a774220d6ddaa4c4196595c05eb5acf1d200b3da
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-14 21:42:17 -0400

    Added styling to posts and post holder

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Blog.css';
 
 const Blog = (props) => {
   const posts = props.posts;
-  const postItems = posts.map((p, id) =>
-     <li key={id}>
-       {p.name}
+  const postItems = posts.map((p) =>
+     <li className="blog-item" key={p.id}>
+       <Link to={"/blog/" + p.slug}>{p.name}</Link>
      </li>
   );
   return (
-    <ul>
-      {postItems}
-    </ul>
+    <div>
+      <ul className="blog-posts">
+        {postItems}
+      </ul>
+    </div>
   );
 }
 
