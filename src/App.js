45f5672a742acd6267112ec00749281ef031532a
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-16 00:52:18 -0400

    Removed routes from main component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 
 //Component imports
-import About from './components/about/About';
-import BlogContainer from './components/blog/BlogContainer';
-import BlogItem from './components/blog/BlogItem';
 import Header from './components/header/Header';
-import Home from './components/home/Home';
 import Footer from './components/footer/Footer';
-import Login from './components/login/Login';
+import Main from './components/main/Main';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -17,11 +13,7 @@ class App extends Component {
       <Router>
         <div>
           <Header headerText="React Blog CMS"/>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route exact path="/blog"  component={BlogContainer} />
-          <Route path="/blog/:slug" component={BlogItem}/>
-          <Route path="/login" component={Login} />
+          <Main />
           <Footer footerText="&copy; Copyright 2017"/>
         </div>
     </Router>
