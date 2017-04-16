b1bd6e0aa1dcfb27cabf73eb939e1c1eed7df28f
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-16 01:04:55 -0400

    Added semi colon for clarity

--- src/components/blog/BlogItem.js
+++ src/components/blog/BlogItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const BlogItem = (props) => {
+  {console.log(props)}
   return (
     <div>
-      <h1 className="text-center">This is the blog item view for {props.match.params.slug}</h1>
+      <h1 className="text-center">This is the blog item view for: {props.match.params.slug}</h1>
     </div>
   );
 }
