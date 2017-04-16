15d9ef63f0d3020fc4c8962848c83b5577e99809
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-16 17:35:50 -0400

    Added route for statistics

--- src/components/admin/AdminRoutes.js
+++ src/components/admin/AdminRoutes.js
@@ -4,12 +4,14 @@ import Dashboard from './dashboard/Dashboard';
 import ListPosts from './blog/ListPosts';
 import CreatePost from './blog/CreatePost';
 import UpdatePost from './blog/UpdatePost';
+import Statistics from './dashboard/statistics/Statistics';
 
 const AdminRoutes = () => {
   return (
     <Switch>
       <Route exact path="/admin/dashboard" component={Dashboard} />
       <Route path="/admin/dashboard/list" component={ListPosts} />
+      <Route path="/admin/dashboard/statistics" component={Statistics} />
       <Route path="/admin/dashboard/create" component={CreatePost} />
       <Route path="/admin/dashboard/update/:id" component={UpdatePost} />
     </Switch>
