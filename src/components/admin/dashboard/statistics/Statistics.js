742b592e415e2dc8fa0aab39cfc4d883b6f4fd22
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-16 17:37:18 -0400

    Created preliminary statistics component

---
+++ src/components/admin/dashboard/statistics/Statistics.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Statistics.css';
+
+class Statistics extends Component {
+  render () {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="text-center">Statistics</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Statistics;
