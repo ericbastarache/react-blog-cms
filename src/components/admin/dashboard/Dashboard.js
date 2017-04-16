07ceeb9240fb7bee47a87bf1aa1bf186269e6b83
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-16 17:37:06 -0400

    Updated admin dashboard component to use reusable component

--- src/components/admin/dashboard/Dashboard.js
+++ src/components/admin/dashboard/Dashboard.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import ActionContainer from '../blog/ActionContainer';
+import '../blog/Blog.css';
 import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1 className="text-center">This is the Admin Dashboard</h1>
-      <ul>
-        <li>
-          <Link to="/admin/dashboard/create">Create Post</Link>
-        </li>
-      </ul>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-3">
+          <ActionContainer actionText="List Posts" actionLinkText="list"/>
+        </div>
+        <div className="col-md-3">
+          <ActionContainer actionText="View Statistics" actionLinkText="statistics"/>
+        </div>
+        <div className="col-md-3">
+          <ActionContainer actionText="Some Dummy Text" actionLinkText="dummy"/>
+        </div>
+        <div className="col-md-3">
+          <ActionContainer actionText="Some Dummy Text" actionLinkText="dummy"/>
+        </div>
+      </div>
     </div>
   );
 }
