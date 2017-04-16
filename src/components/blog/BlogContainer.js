950b9d4ce7221f17eba599232413a70e23c4c7a0
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-16 01:36:17 -0400

    Added dummy content

--- src/components/blog/BlogContainer.js
+++ src/components/blog/BlogContainer.js
@@ -10,20 +10,27 @@ export class BlogContainer extends Component {
         {
           id: 1,
           slug: 'test-post',
-          name: 'Test post'
+          name: 'Test post',
+          content: 'TEST POST CONTENT: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         {
           id: 2,
           slug: 'another-test-post',
-          name: 'Another test post'
+          name: 'Another test post',
+          content: 'ANOTHER TEST POST CONTENT: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         {
           id: 3,
           slug: 'will-the-test-posts-ever-end',
-          name: 'Will the test posts ever end?'
+          name: 'Will the test posts ever end?',
+          content: 'WILL IT EVER END CONTENT: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         }
       ]
     }
+  }
+
+  componentWillMount () {
+    
   }
 
   render () {
