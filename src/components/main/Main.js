0170d89d2696e4d7d8bd9caf775d99f2f712f061
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-16 00:53:09 -0400

    Created main component to handle routing

---
+++ src/components/main/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from '../home/Home';
+import About from '../about/About';
+import BlogContainer from '../blog/BlogContainer';
+import BlogItem from '../blog/BlogItem';
+import Login from '../login/Login';
+
+const Main = () => {
+  return(
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route exact path="/blog"  component={BlogContainer} />
+      <Route path="/blog/:slug" component={BlogItem}/>
+      <Route path="/login" component={Login} />
+    </Switch>
+  );
+}
+
+export default Main;
