da77cab5a1e4a47f4f4901ed7f4d747225647e41
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-16 17:13:42 -0400

    Created admin list posts component

---
+++ src/components/admin/blog/ListPosts.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ListPosts = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h3 className="text-center">All Posts</h3>
+          <table className="table table-striped table-responsive">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Content</th>
+                <th>Tags</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Test Post</td>
+                <td>TEST POST: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+                <td>test, post, content</td>
+                <td><Link to={"/admin/dashboard/update/1"} className="btn btn-success"><i className="fa fa-pencil"></i></Link></td>
+                <td><Link to={"/admin/dashboard/delete/1"} className="btn btn-danger"><i className="fa fa-trash-o"></i></Link></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ListPosts;
