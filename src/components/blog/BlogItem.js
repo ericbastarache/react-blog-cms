50c107fe6f26fb243956420ad6864eb6089a7c04
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-16 01:05:21 -0400

    Removed console log statement

--- src/components/blog/BlogItem.js
+++ src/components/blog/BlogItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const BlogItem = (props) => {
-  {console.log(props)}
   return (
     <div>
       <h1 className="text-center">This is the blog item view for: {props.match.params.slug}</h1>
