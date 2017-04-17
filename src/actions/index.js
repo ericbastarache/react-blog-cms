0627ec7356b1af5df2eddb3c062b7c587ff05bf1
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-16 20:40:39 -0400

    Added fetchPost method action

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,13 @@ import axios from 'axios';
 //Posts actions
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const FETCH_POST = 'FETCH_POST';
+
+const API_URL = 'http://localhost:3001/api';
+
+export const fetchPosts = () => {
+  const request = axios.get(`${API_URL}/posts`);
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  }
+}
