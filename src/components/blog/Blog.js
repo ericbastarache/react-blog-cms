6c11ba51a6b5170a93d2fd1a2138fa7b409c5cd4
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-16 20:41:00 -0400

    Converted blog to component to render posts from redux store

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+//import { fetchPosts } from '../../actions';
+//import { connect } from 'react-redux';
 import './Blog.css';
 
-const Blog = (props) => {
-  const posts = props.posts;
-  const postItems = posts.map((p) =>
-    <li className="blog-item" key={p.id}>
-      <Link to={"/blog/" + p.slug}>{p.name}</Link>
-    </li>
-  );
-  return (
-    <ul className="blog-posts">
-      {postItems}
-    </ul>
-  );
+class Blog extends Component {
+  render () {
+    return (
+      <ul className="blog-posts">
+        {this.renderPosts()}
+      </ul>
+    );
+  }
+
+  renderPosts () {
+    const posts = this.props.posts;
+    return posts.map((p) => {
+      return(
+        <li className="blog-item" key={p.id}>
+          <Link to={"/blog/" + p.slug}>{p.name}</Link>
+        </li>
+      );
+    });
+  }
+
+  componentWillMount () {
+    //this.props.fetchPosts();
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  };
 }
 
 export default Blog;
+//export default connect(mapStateToProps, {fetchPosts})(Blog);
