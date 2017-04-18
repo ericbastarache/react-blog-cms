6cc62669fdd52d6f64e347c64c3f51115e6c7af7
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-18 16:02:05 -0400

    Switched to express

--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const logger = require('koa-logger');
-const Router = require('koa-router');
-const api = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const routes = require('./routes');
 const port = process.env.PORT || 3001;
 
-const app = new Koa();
-const router = new Router();
+const app = express();
 
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(api.routes());
+app.use('/api', routes);
 
 app.listen(port, () => {
-  console.log('Server running on http://localhost:' + port);
+  console.log(`SERVER running on http://localhost:${port}`)
 });
