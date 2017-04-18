d318300745c2cb1b11063d1aaa6c2308d586c548
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-18 17:05:49 -0400

    Created preliminary posts controller

--- server/controllers/PostsController.js
+++ server/controllers/PostsController.js
@@ -1,4 +1,15 @@
+exports.get_posts = (req, res) => {
+  res.send('This is POSTS');
+}
 
-module.exports.get_posts = (ctx, next) => {
-  ctx.body = "This is POSTS";
+exports.get_post = (req, res) => {
+  res.send('This is a POST by ID');
+}
+
+exports.create_post = (req, res) => {
+  res.send("This is CREATE post");
+}
+
+exports.update_post = (req, res) => {
+  res.send("This is UPDATE post");
 }
