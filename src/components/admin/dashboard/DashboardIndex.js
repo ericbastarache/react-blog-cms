84b270429175beb898f555006cc5631590e86800
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-20 15:18:33 -0400

    Removed markup for dashboard for now

--- src/components/admin/dashboard/DashboardIndex.js
+++ src/components/admin/dashboard/DashboardIndex.js
@@ -4,32 +4,8 @@ import { Link } from 'react-router-dom';
 
 const DashboardIndex = () => {
   return (
-    <div id="wrapper">
-      <div id="sidebar-wrapper">
-        <ul className="sidebar-nav">
-          <li>
-            <Link to="/admin/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/admin/dashboard/posts">Posts</Link>
-          </li>
-          <li>
-            <Link to="/admin/dashboard/posts/create">Create Post</Link>
-          </li>
-          <li>
-            <a href="#">Events</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
+    <div>
+      <h1>This is Dashboard Index</h1>
     </div>
   );
 }
