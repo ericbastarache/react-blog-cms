bb1d58f2d03d820d89d827c667f099bb699680f9
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-19 22:44:15 -0400

    Added update route

--- src/components/admin/dashboard/Dashboard.js
+++ src/components/admin/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import BlogList from './blog/BlogList';
 import CreatePost from './blog/CreatePost';
+import UpdatePost from './blog/UpdatePost';
 
 const Dashboard = () => {
   return (
@@ -9,6 +10,7 @@ const Dashboard = () => {
       <Route path="/admin/dashboard" component={DashboardIndex} />
       <Route path="/admin/posts" component={BlogList} />
       <Route path="/admin/posts/create" component={CreatePost} />
+      <Route path="/admin/posts/update/:id" component={UpdatePost} />
     </Switch>
   );
 }
