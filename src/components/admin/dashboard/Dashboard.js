fa33bb620cc69d029198bd41f81ec05b79683d19
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-20 06:47:30 -0400

    Updated dashboard routes

--- src/components/admin/dashboard/Dashboard.js
+++ src/components/admin/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import DashboardIndex from './DashboardIndex';
 import BlogList from './blog/BlogList';
 import CreatePost from './blog/CreatePost';
 import UpdatePost from './blog/UpdatePost';
@@ -8,9 +9,9 @@ const Dashboard = () => {
   return (
     <Switch>
       <Route path="/admin/dashboard" component={DashboardIndex} />
-      <Route path="/admin/posts" component={BlogList} />
-      <Route path="/admin/posts/create" component={CreatePost} />
-      <Route path="/admin/posts/update/:id" component={UpdatePost} />
+      <Route path="/admin/dashboard/posts" component={BlogList} />
+      <Route path="/admin/dashboard/posts/create" component={CreatePost} />
+      <Route path="/admin/dashboard/posts/update/:id" component={UpdatePost} />
     </Switch>
   );
 }
