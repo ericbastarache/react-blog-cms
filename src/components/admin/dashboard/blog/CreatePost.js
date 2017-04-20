a1179b20a866f84002814dc765a94488c7f7ae1f
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-19 22:40:05 -0400

    Created preliminary create post component

---
+++ src/components/admin/dashboard/blog/CreatePost.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CreatePost extends Component {
+  render() {
+    return (
+      <h1>Create Post</h1>
+    );
+  }
+}
+
+export default CreatePost;
