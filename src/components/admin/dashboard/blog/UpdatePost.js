ef7f1d278e0eaf7131f5535b9e0440f88294ccca
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-19 22:40:12 -0400

    Created preliminary update post component

---
+++ src/components/admin/dashboard/blog/UpdatePost.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class UpdatePost extends Component {
+  render() {
+    return(
+      <h1>This is Update Post</h1>
+    );
+  }
+}
+
+export default UpdatePost;
