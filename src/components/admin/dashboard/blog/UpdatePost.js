0aae7163aacfae7c01dd0bb6675bed46746be8d9
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-20 06:47:51 -0400

    Created preliminary markup for update component

--- src/components/admin/dashboard/blog/UpdatePost.js
+++ src/components/admin/dashboard/blog/UpdatePost.js
@@ -3,8 +3,29 @@ import React, { Component } from 'react';
 class UpdatePost extends Component {
   render() {
     return(
-      <h1>This is Update Post</h1>
+      <div id="page-content-wrapper">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <form onSubmit={this.handleSubmit} className="form-horizontal">
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
     );
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value
+    });
   }
 }
 
