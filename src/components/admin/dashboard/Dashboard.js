c2d1611a239f87c8639497eef93ef1d2d1616706
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-19 22:40:22 -0400

    Updated routes for admin dashboard

--- src/components/admin/dashboard/Dashboard.js
+++ src/components/admin/dashboard/Dashboard.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import BlogList from './blog/BlogList';
+import CreatePost from './blog/CreatePost';
 
 const Dashboard = () => {
   return (
     <Switch>
       <Route path="/admin/dashboard" component={DashboardIndex} />
+      <Route path="/admin/posts" component={BlogList} />
+      <Route path="/admin/posts/create" component={CreatePost} />
     </Switch>
   );
 }
