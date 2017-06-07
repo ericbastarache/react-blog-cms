8e20ec188b9537be44e497a634a2f2984b004624
Eric Bastarache <eric_bastarache@hotmail.com>
2017-06-07 19:57:47 -0400

    Updated blog item component

--- src/components/blog/BlogItem.js
+++ src/components/blog/BlogItem.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const BlogItem = (props) => {
+// const BlogItem = (props) => {
+//
+//   return (
+//     <div>
+//
+//     </div>
+//   );
+// }
+class BlogItem extends Component {
+  render () {
+    return (
+      <div>
+        { this.renderItem() }
+      </div>
+    )
+  }
+  renderItem () {
+    const posts = this.props.posts.posts;
 
-  return (
-    <div>
-      <h1 className="text-center">{props.match.params.slug}</h1>
-      <p></p>
-    </div>
-  );
+    return posts.map((post) => {
+      console.log(post);
+      if(post.slug === this.props.match.params.slug) {
+        return (
+           <div key={post.id}>
+             <h1>{post.name}</h1>
+             <p>{post.content}</p>
+           </div>
+         )
+       }
+    })
+  }
 }
 
-export default BlogItem;
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps)(BlogItem);
