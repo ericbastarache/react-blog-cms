815cdc6977ebb168d31de7e666a8831b8c93f8c6
Eric Bastarache <eric_bastarache@hotmail.com>
2017-07-23 22:34:44 -0400

    Updated blog component

--- src/components/admin/dashboard/blog/CreatePost.js
+++ src/components/admin/dashboard/blog/CreatePost.js
@@ -4,7 +4,16 @@ class CreatePost extends Component {
   render() {
     return (
       <h1 className="text-center">Create Post</h1>
+      <form className="form-horizontal">
+        <input type="text" className="form-control" onChange={this.handleChange} />
+      </form>
     );
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 }
 
